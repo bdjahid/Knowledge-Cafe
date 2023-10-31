8d37d7d9c6f6d328d01070d002cbe291456b5b45
Your Name <you@example.com>
2023-10-31 21:38:36 +0600

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Blogs from "./components/Blogs"
+import Bookmarks from "./components/Bookmarks"
 import Header from "./components/Header/Header"
 
 
@@ -7,6 +9,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className="md:flex">
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
