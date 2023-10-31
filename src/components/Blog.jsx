dd3dffa9850e8b2fa856cf8f2ae1b3ca6aa11559
Your Name <you@example.com>
2023-10-31 22:30:39 +0600

    blog hashtag

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,9 +1,28 @@
 import PropTypes from 'prop-types'
 const Blog = ({ blog }) => {
     console.log(blog)
+    const { title, cover, author, img, date, readingtime, hashtags } = blog;
     return (
         <div>
-
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex'>
+                    <img className='w-14' src={img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{readingtime}  min read</span>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hashtag, idx) => <span key={idx}><a href=''>#{hashtag}</a></span>)
+                }
+            </p>
         </div>
     );
 };
